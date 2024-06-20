e0d00871c41621f187a4ac0dd8feaaafc3c6ed10
amirvaknin <amir6661@gmail.com>
2024-06-20 21:37:42 +0200

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
 const router = require("express").Router();
+const bcryptjs = require("bcryptjs")
+const saltRounds = 10
+const User = require('../models/User.model');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+/* GET signup page */
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+/* POST signup page */
+router.post("/signup", (req, res, next) => {
+  const { username, email, password } = req.body;
+ 
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        username: username,
+        email: email,
+        passwordHash: hashedPassword
+      });
+    })
+    .then(userFromDB => {
+      res.redirect('/userProfile');
+    })
+    .catch(error => next(error));
+});
+
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
 
 module.exports = router;
