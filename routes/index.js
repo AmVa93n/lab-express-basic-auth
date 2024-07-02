b306cc90cee2f8d064331c5ff78a397a403d5831
amirvaknin <amir6661@gmail.com>
2024-07-02 19:22:26 +0200

    iteration 2 done

--- routes/index.js
+++ routes/index.js
@@ -33,9 +33,51 @@ router.post("/signup", (req, res, next) => {
     .catch(error => next(error));
 });
 
-router.get('/userProfile/:username', (req, res) => {
-  const username = req.params.username
-  res.render('users/user-profile', {username})
+router.get('/userProfile', (req, res) => {
+  res.render('users/user-profile', { userInSession: req.session.currentUser });
+});
+
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+
+// POST login route ==> to process form data
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+  const { email, password } = req.body;
+ 
+  if (email === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, email and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        console.log("Email not registered. ");
+        res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // when we introduce session, the following line gets replaced with what follows:
+        // res.render('users/user-profile', { user });
+ 
+        //******* SAVE THE USER IN THE SESSION ********//
+        req.session.currentUser = user;
+        res.redirect('/userProfile');
+      } else {
+        console.log("Incorrect password. ");
+        res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
